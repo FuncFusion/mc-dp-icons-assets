25270f66b6744e63ce82fa8ce9bf137fa09774c3
bth123 <m1207pro@gmail.com>
2025-10-19 13:05:12 +0300

    🌌 Some icons; holy true slashes

--- scripts/cropupscale.py
+++ scripts/cropupscale.py
@@ -42,6 +42,6 @@ def crop_and_upscale_images(input_folder, output_folder, target_size=1000):
                     print(f"Processed and saved: {output_path}")
 
 if __name__ == "__main__":
-    input_dir = "..\\icons\\future\\files\\"
-    output_dir = "..\\icons\\future\\us"
+    input_dir = "../icons/future/files/"
+    output_dir = "../icons/future/us"
     crop_and_upscale_images(input_dir, output_dir)
