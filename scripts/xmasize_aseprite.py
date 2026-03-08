10fa0460a6f15d8b36572504c07e2933d65de541
bth123 <m1207pro@gmail.com>
2026-03-08 11:28:40 +0200

    smol tweaks

--- scripts/xmasize_aseprite.py
+++ scripts/xmasize_aseprite.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
 	from os.path import abspath
 	import webbrowser
 
-	icons_path = "../icons/future/"
+	icons_path = "../icons/current/"
 	icons_path += "/" if not icons_path.endswith("/") else ""
 
 	for root, __, files in walk(icons_path):
