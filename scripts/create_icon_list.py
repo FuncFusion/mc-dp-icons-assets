c9e8de10234a7f6799603789d599c2e9d0dc6a0a
bth123 <m1207pro@gmail.com>
2026-07-17 02:00:22 +0300

    upscales

---
+++ scripts/create_icon_list.py
@@ -0,0 +1,12 @@
+import os
+
+
+icons = []
+for _, __, files in os.walk("../icons/current/"):
+	for file in files:
+		name = ".".join(file.split(".")[:-1])
+		if name not in icons:
+			icons.append(name)
+
+with open("../icons/list.txt", "w") as f:
+	f.write("\n".join(icons))
