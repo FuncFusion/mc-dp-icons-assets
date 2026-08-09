263f08cec9b232d3de1cd41de822fbe9ff7dafd0
bth123 <m1207pro@gmail.com>
2026-08-09 16:15:28 +0300

    📜 icon list update

--- scripts/create_icon_list.py
+++ scripts/create_icon_list.py
@@ -2,11 +2,11 @@ import os
 
 
 icons = []
-for _, __, files in os.walk("../icons/current/"):
+for _, __, files in os.walk("icons/current/"):
 	for file in files:
 		name = ".".join(file.split(".")[:-1])
 		if name not in icons:
 			icons.append(name)
 
-with open("../icons/list.txt", "w") as f:
+with open("icons/.list", "w") as f:
 	f.write("\n".join(icons))
